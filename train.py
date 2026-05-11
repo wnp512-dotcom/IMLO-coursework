a64de3763d0b77f01b1bfbbe93e4b0a88e06beb6
wnp512-dotcom <wnp512@york.ac.uk>
2026-05-11 12:54:52 +0100

    Update train.py

--- train.py
+++ train.py
@@ -101,28 +101,3 @@ batch_size = 64
 epochs = 5
 optimizer = optim.SGD(net.parameters(), lr=0.01)
 criterion = nn.CrossEntropyLoss()
-
-def train_loop(dataloader, model, criterion, optimizer):
-    size = len(dataloader.dataset)
-    # Set the model to training mode - important for batch normalization and dropout layers
-    # Unnecessary in this situation but added for best practices
-    model.train()
-    for batch, (X, y) in enumerate(dataloader):
-        # Compute prediction and loss
-        pred = model(X)
-        loss = criterion(pred, y)
-
-        # Backpropagation
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
-
-        if batch % 100 == 0:
-            loss, current = loss.item(), batch * batch_size + len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
-
-
-for t in range(epochs):
-    print(f"Epoch {t+1}\n-------------------------------")
-    train_loop(train_dataloader, net, criterion, optimizer)
-print("Done!")
